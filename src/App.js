a36d2f1b66ba46d070f255c69ba7ef262f387ff1
Aflan-63 <ifaz.mobile.dev@gmail.com>
2020-11-22 23:49:55 +0600

    Component Part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
+
+import { Navbar, NavbarBrand, Button } from 'reactstrap';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import MenuComponent from './components/MenuComponent';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary" className="">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+
+          </div>
+        </Navbar>
+        <MenuComponent></MenuComponent>
+      </div>
+    )
+  }
 }
 
-export default App;
